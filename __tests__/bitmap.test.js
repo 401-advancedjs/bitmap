40238945ea4e5289261b66e370062996a871b0df
mestock <MelissaStock44@gmail.com>
2019-05-18 23:58:34 -0700

    Started testing

--- __tests__/bitmap.test.js
+++ __tests__/bitmap.test.js
@@ -1,13 +1,27 @@
 'use strict';
 
 const fs = require('fs');
+const Bitmap = require('../lib/bitmap.js');
 const transforms = require('../lib/transforms');
+
 
 describe('Bitmap', () => {
   it('should create an object with details about the selected BM', () => {
-
+    let test = new Bitmap('../assets/baldy.bmp');
+    fs.readFile('../assets/baldy.bmp', buffer => {
+      test.parse(buffer);
+      expect(test.type).toEqual('BM');
+    });
   });
   it('should transform the BM when a valid operation is inputed', () => {
+    let check = new Bitmap('../assets/baldy.bmp');
+    let test = new Bitmap('../assets/baldy.bmp');
+    fs.readFile('../assets/baldy.bmp', buffer => {
+      test.parse(buffer);
+      check.parse(buffer);
+      transforms['bluescale'](test);
+      expect(test.buffer.slice(1146)).not.toEqual(check.buffer.slice(1146));
+    });
 
   });
   it('should throw an error if an invalid path is inputed', () => {
--- index.js
+++ index.js
@@ -1,68 +1,23 @@
 'use strict';
 
 const fs = require('fs');
-const transforms = require('./lib/transforms.js');
+const Bitmap = require('./lib/bitmap.js');
+const fileReader = require('./lib/file-reader.js');
 
-/**
- * Bitmap -- receives a file name, used in the transformer to note the new buffer
- * @param filePath
- * @constructor
- */
-function Bitmap(filePath) {
-  this.file = filePath;
-}
-
-/**
- * Parser -- accepts a buffer and will parse through it, according to the specification, creating object properties for each segment of the file
- * @param buffer
- */
-Bitmap.prototype.parse = function(buffer) {
-  this.buffer = buffer;
-  this.type = buffer.toString('utf-8', 0, 2);
-  this.file_size = buffer.readInt16LE(2);
-  this.start_of_pixel = buffer.readInt16LE(10);
-  this.file_width = buffer.readInt32LE(18);
-  this.file_height = buffer.readInt32LE(22);
-  this.bits_per_pix = buffer.readInt16LE(28);
-  
-};
-
-
-/**
- * Transform a bitmap using some set of rules. The operation points to some function, which will operate on a bitmap instance
- * @param operation
- */
-
-Bitmap.prototype.transform = function(operation) {
-  // This is really assumptive and unsafe
-  transforms[operation](this);
-  this.newFile = this.file.replace(/\.bmp/, `.${operation}.bmp`);
-};
-
-
-// ------------------ GET TO WORK ------------------- //
 
 function transformWithCallbacks() {
-
   fs.readFile(file, (err, buffer) => {
-
     if (err) {
       throw err;
     }
-
     bitmap.parse(buffer);
-
     bitmap.transform(operation);
-
-    // Note that this has to be nested!
-    // Also, it uses the bitmap's instance properties for the name and thew new buffer
     fs.writeFile(bitmap.newFile, bitmap.buffer, (err, out) => {
       if (err) {
         throw err;
       }
       console.log(`Bitmap Transformed: ${bitmap.newFile}`);
     });
-
   });
 }
 
